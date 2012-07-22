d7cb7f42e588de6352327a2fcf23ac27c4264532
Paul Tagliamonte <tag@pault.ag>
2012-07-22 15:31:06 -0400

    Bulking out

--- organized/importers/github.py
+++ organized/importers/github.py
@@ -2,11 +2,30 @@
 # the terms and conditions of the Expat license
 
 from organized.importer import Importer
+from organized.util import create_id, log
 
 class GitHub(Importer):
     def __init__(self, owner, project):
+        log("Created github instance. %s/%s" % (
+            owner, project
+        ))
         self._owner = owner
         self._project = project
 
-    def update(self):
+
+    def _get_issue_page(self, page):
+        log("Getting page %s" % (page))
         pass
+
+
+    def _get_all_issues(self):
+        log("Getting issues.")
+        counter = 1
+        numup = -1
+        while numup != 0:
+            numup = self._get_issue_page(counter)
+            counter += 1
+
+
+    def update(self):
+        self._get_all_issues()
--- organized/util.py
+++ organized/util.py
@@ -1,6 +1,11 @@
 # Copyright (c) Paul Tagliamonte <paultag@debian.org>, 2012 under the
 # terms and conditions of the Expat license.
 
+import logging
+
+logger = logging.getLogger('organized')
+
+
 def create_id(prefix, klass, package, idno):
     return "%s-%s-%s-%s" % (
         prefix,
@@ -8,3 +13,6 @@ def create_id(prefix, klass, package, idno):
         package,
         idno
     )
+
+def log(message):
+    logger.info(message)
