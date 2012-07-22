d473d05307e5aca1c54400c69069f40b4b8dbb1f
Paul Tagliamonte <tag@pault.ag>
2012-07-22 14:43:04 -0400

    small copyright stuff

--- organized/__init__.py
+++ organized/__init__.py
@@ -1,3 +1,5 @@
+# Copyright (c) Paul Tagliamonte <paultag@debian.org>, 2012 under the
+# terms and conditions of the Expat license.
 
 __appname__ = "organized"
 __version__ = "0.1~pre0"
--- organized/db.py
+++ organized/db.py
@@ -1,3 +1,6 @@
+# Copyright (c) Paul Tagliamonte <paultag@debian.org>, 2012 under the
+# terms and conditions of the Expat license.
+
 from pymongo import Connection
 
 connection = Connection('localhost', 27017)
