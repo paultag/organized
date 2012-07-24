874eddcbfab8a7b57d7d6ef1ae8adda0e3cd4994
Paul Tagliamonte <tag@pault.ag>
2012-07-23 20:08:56 -0400

    Building out some website stuff.

--- site/app.py
+++ site/app.py
@@ -2,13 +2,24 @@
 # and conditions of the Expat license, a copy of which should be given to you
 # with the source of this application.
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from organized.db import db
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template('index.html', **{
+    return render_template('index.html', **{})
+
+
+@app.route('/project/<project>/')
+def project(project=None):
+    probj = db.projects.find_one({"_id": project})
+    if probj is None:
+        return redirect(url_for('index'))
+
+    return render_template('project.html', **{
+        "project": probj
     })
 
 
